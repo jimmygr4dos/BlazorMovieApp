b5ed78d08a031db255b631c4e0ed5f39c933a64c
Jimmy Grados <died_sistemas25@minedu.gob.pe>
2024-04-16 00:27:14 -0500

    Add project files.

---
+++ BlazorMovieApp/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        [Range(0, 100)]
+        public decimal Price { get; set; }
+    }
+}
